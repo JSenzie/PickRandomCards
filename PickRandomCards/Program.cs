c95e18c43be17d4a89ae52e280220d41e07d475d
joseph <jsensback@gmail.com>
2024-08-22 14:07:04 -0700

    Add project files.

---
+++ PickRandomCards/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace PickRandomCards
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("How many cards would you like: ");
+            string? numberOfCardsChosen = Console.ReadLine();
+            bool parseSuccessful = int.TryParse(numberOfCardsChosen, out int cardNumber);
+
+            while (!parseSuccessful)
+            {
+                Console.Write("Enter a valid number of cards you would like: ");
+                numberOfCardsChosen = Console.ReadLine();
+                parseSuccessful = int.TryParse(numberOfCardsChosen, out cardNumber);
+            }
+
+            string[] returnedCards = CardPicker.PickSomeCards(cardNumber);
+            foreach (string card in returnedCards) Console.WriteLine(card);
+        }
+    }
+}
